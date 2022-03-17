bd1612d4918d53451c907178efe37732ca6b9ebf
lee <leejihyun0324@gmail.com>
2022-03-17 23:33:00 +0900

    java study 12

--- section12/integerClass.java
+++ section12/integerClass.java
@@ -2,7 +2,7 @@ package section12;
 
 public class integerClass {
     public static void main(String[] args) {
-        Integer IntVar1 = new Integer(123); // Integer을 생성해 IntVar1 객체에 123저장
+        Integer IntVar1 = Integer.valueOf("123"); // Integer을 생성해 IntVar1 객체에 123저장
         int nVar = IntVar1.intValue(); // IntVar1 객체의 값을 정수값으로
         System.out.println(nVar);
 
